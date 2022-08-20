4b397967b120f97cae9d76a98c129b4f2bf4f874
orenanbarros <renanalmeidadebarros@gmail.com>
2022-08-20 18:28:53 -0300

    commit inicial do projeto miniautorizador

---
+++ src/main/java/com/vr/miniautorizadorvr/MiniautorizadorvrApplication.java
@@ -0,0 +1,13 @@
+package com.vr.miniautorizadorvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniautorizadorvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniautorizadorvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vr/miniautorizadorvr/MiniautorizadorvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.miniautorizadorvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniautorizadorvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
